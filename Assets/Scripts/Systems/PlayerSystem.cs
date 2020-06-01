4fd9c1f22cbfe242b327b11a12db1e003f8b003d
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-06-01 17:10:05 +0200

    PlayerSystem prototype
    Added PlayerData, PlayerSystem

---
+++ Assets/Scripts/Systems/PlayerSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class PlayerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var xAxis = Input.GetAxis("Horizontal");
+        var yAxis = Input.GetAxis("Vertical");
+        var delataTime = Time.DeltaTime;
+
+
+        Entities
+            .WithAll<PlayerData>()
+            .ForEach((ref MoveData move) =>
+            {
+                move.movingDirection = new float3(xAxis, 0, yAxis);
+            }).Schedule();
+    }
+}

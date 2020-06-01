18683abd773977c9065d75f6d9ee2d74cd02955b
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-06-01 16:42:53 +0200

    move system
    MoveSystem and MoveData created

---
+++ Assets/Scripts/Components/MoveData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct MoveData : IComponentData
+{
+    public float movingSpeed;
+    public float3 movingDirection;
+}
---
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+public class MoveSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref PhysicsVelocity physicsVelocity, in MoveData moveData) =>
+        {
+            var move = moveData.movingDirection * moveData.movingSpeed;
+            physicsVelocity.Linear = move;
+        }).Schedule();
+    }
+}

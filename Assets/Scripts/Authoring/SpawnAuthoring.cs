1efd0cf57b64adbd4d16d858a355d1036ff078bf
MarekHas <45779384+MarekHas@users.noreply.github.com>
2020-06-03 12:00:20 +0200

    Spawn system
    Created spawn mechanic

---
+++ Assets/Scripts/Authoring/SpawnAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class SpawnAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject GameObjectSpawned;
+
+    public void Convert(Entity entity, EntityManager entityManager, GameObjectConversionSystem conversionSystem)
+    {
+        entityManager.AddComponentData(entity, new SpawnData() { PrefabSpawned = conversionSystem.GetPrimaryEntity(GameObjectSpawned) });
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(GameObjectSpawned);
+    }
+}
---
+++ Assets/Scripts/Systems/SpawnSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class SpawnSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref SpawnData spawnData, in Translation translation, in Rotation rotation) =>
+        {
+            if (!EntityManager.Exists(spawnData.ObjectSpawned))
+            {
+                spawnData.ObjectSpawned = EntityManager.Instantiate(spawnData.PrefabSpawned);
+                EntityManager.SetComponentData(spawnData.ObjectSpawned, translation);
+                EntityManager.SetComponentData(spawnData.ObjectSpawned, rotation);
+            }
+
+        }).WithStructuralChanges().Run();
+    }
+}
